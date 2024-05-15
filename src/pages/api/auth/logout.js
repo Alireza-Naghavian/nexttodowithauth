29312613f4f2509314a9c88d56ec14b3b80e6fbd
alireza <alirezanghngh1379@gmail.com>
2024-05-15 10:09:27 +0430

    logout-api

---
+++ src/pages/api/auth/logout.js
@@ -0,0 +1,26 @@
+import { serialize } from "cookie";
+import connectedToDB from "../../../../config/db";
+import { verifyToken } from "../../../../utils/authUtils";
+
+const logoutHanlder = async (req, res) => {
+  if (req.method !== "GET") return false;
+  try {
+    connectedToDB();
+    const { token } = req.cookies;
+    const tokenPayload = verifyToken(token);
+
+    if (!token || !tokenPayload)
+      return res.status(401).json({ message: "شما لاگین نیستید" });
+
+    return res
+      .setHeader(
+        "Set-Cookie",
+        serialize("token", "", {
+          path: "/",
+          maxAge: 0,
+        })
+      )
+      .json({ message: "خروج با موفقیت انجام شد !" });
+  } catch (error) {}
+};
+export default logoutHanlder;
